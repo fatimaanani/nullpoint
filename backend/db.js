d1bbe01e02e10611ac90659103a9a9a5608935c3
fatima <fatima_anani_02@hotmail.com>
2026-01-05 01:59:36 +0200

    updates

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nullpoint",
-  port: 3306,  
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10
 });
